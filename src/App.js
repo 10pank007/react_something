1a4f22661f5abb8da2fac1d71d69137e154a1ecb
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-10-27 14:45:52 +0300

    lesson3 part2 createRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {createRef} from "react";
 
 function App() {
+    let login = createRef();
+    let password = createRef();
+    function onSubmit(e) {
+        e.preventDefault();
+       console.log(login.current.value)
+       console.log(password.current.value)
+    }
   return (
     <div className="App">
       Hello world!!! Ukraine win war in 2023
+
+        <form action={""} onSubmit={onSubmit}>
+            <input type={"text"} name={"login"} ref={login}/>
+            <input type={"text"} name={"password"} ref={password}/>
+            <button>log in</button>
+        </form>
     </div>
   );
 }
