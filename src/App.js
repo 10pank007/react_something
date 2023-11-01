270e1492e72db97b63d4e27ed32a33004cb0299c
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-11-01 16:01:43 +0200

    lesson6 children

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {MyComponent} from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
       Hello world!!! Ukraine win war in 2023
+        <MyComponent>
+            <h1>hello</h1>
+        </MyComponent>
     </div>
   );
 }
