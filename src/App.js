9f0ad994f165c4bab632982741208ae8371c1d62
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-11-22 18:35:46 +0200

    part 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import {Test} from "./Test";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
+    console.log("constructor");
     const [flag, setFlag] = useState(true);
+    useEffect(() => {
+        console.log("componentDidMount");
+        return () => {
+            console.log("componentWillUnmount");
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log("componentDidUpdate")
+    }, [flag]);
   return (
     <div className="App">
       Hello world!!! Ukraine win war in 2023
-        {flag && <Test b={25}/>}
+        {console.log("render")}
+        {/*{flag && <Test b={25}/>}*/}
         <button onClick={()=> setFlag(prevState => !prevState)}>Hide</button>
     </div>
   );
